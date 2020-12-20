1454b53c2ba6396c55ffa8ef30847c7ed44ee635
Daniel Grace <negativeview@gmail.com>
2020-12-19 22:35:16 -0600

    Add zip extension.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM php:8.0.0-fpm
 
+RUN apt-get update \
+    && apt-get install -y libzip-dev \
+    && docker-php-ext-install zip
+
 COPY ./build.sh .
 RUN /bin/sh ./build.sh
--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --install-dir=/bin --filename=composer --version=2.0.8
 rm composer-setup.php
+
